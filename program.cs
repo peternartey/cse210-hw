3e68e78f8314e317dfa32a5595dbb17d2d8e0b9b
peter-nartey <pnartey@byupathway.edu>
2025-03-30 00:57:13 +0000

    cse210-hw

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[]args);
+    {
+    console.write("what is your name?");
+    string first= console.readline();
+    console.write("what is your last name?");
+    string last= console.readline();
+    console.writeLine($"your name is {last}, {first} {last}");
+    
+    }
+}
